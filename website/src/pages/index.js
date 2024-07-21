e5a484d147af96637cd1a6445ed3a1f22acbe74c
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2024-07-20 22:44:38 -0700

    basic styling to main page

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -9,9 +9,12 @@ const properties = {
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
+    <main className={`flex min-h-screen flex-col items-center space-y-9 p-24`}>
+      <h1 className="font-black text-3xl">genpeeps</h1>
+      <p>
+        Automatically generate Notion styled profiles/avatars. Based on
+        openpeeps and opeep.fun
+      </p>
       <div className="w-[200px]">
         <Peep {...properties} />
       </div>
